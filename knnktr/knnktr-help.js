97bccc5ad30b12c3874a098bff5621b0fc7dad15
Wayne Berry <wayneashleyberry@gmail.com>
2012-04-26 12:29:48 +0200

    update help

--- knnktr/knnktr-help.js
+++ knnktr/knnktr-help.js
@@ -7,24 +7,26 @@ program
   .version('0.0.1')
   .parse(process.argv);
 
-                                                              
+var log = console.log;
+
 // ascii madness
-console.log('    _/                            _/        _/                ');
-console.log('   _/  _/    _/_/_/    _/_/_/    _/  _/  _/_/_/_/  _/  _/_/   ');
-console.log('  _/_/      _/    _/  _/    _/  _/_/      _/      _/_/        ');
-console.log(' _/  _/    _/    _/  _/    _/  _/  _/    _/      _/           ');
-console.log('_/    _/  _/    _/  _/    _/  _/    _/    _/_/  _/            ');
+log('    _/                            _/        _/                ');
+log('   _/  _/    _/_/_/    _/_/_/    _/  _/  _/_/_/_/  _/  _/_/   ');
+log('  _/_/      _/    _/  _/    _/  _/_/      _/      _/_/        ');
+log(' _/  _/    _/    _/  _/    _/  _/  _/    _/      _/           ');
+log('_/    _/  _/    _/  _/    _/  _/    _/    _/_/  _/            ');
 
-console.log(color('knnktr commands', 'red'));
-console.log('  ' + color('kp', 'blue') + '      knnktr projects');
-console.log('  ' + color('kk', 'blue') + '      return to current project root directory');
-console.log('  ' + color('kn', 'blue') + '      TODO: create a new project');
+log(color('knnktr commands', 'red'));
+log('  ' + color('kp', 'blue') + '      knnktr projects');
+//log('  ' + color('kn', 'blue') + '      TODO: create a new project');
 
-console.log(color('project tools', 'red'));
-console.log('  ' + color('build', 'blue') + '   runs make in project root directory');
-console.log('  ' + color('deploy', 'blue') + '  runs deploy script in root directory');
+log(color('project tools', 'red'));
+log('  ' + color('build', 'blue') + '   runs make in project root directory');
+log('  ' + color('deploy', 'blue') + '  runs deploy script in root directory');
 
-console.log(color('project aliases', 'red'));
-console.log('  ' + color('css', 'blue') + '     css directory');
-console.log('  ' + color('img', 'blue') + '     images directory');
-console.log('  ' + color('js', 'blue') + '      javascript directory');
+log(color('project aliases', 'red'));
+log('  ' + color('kk', 'blue') + '      return to current project root directory');
+log('  ' + color('gg', 'blue') + '      same as kk');
+log('  ' + color('css', 'blue') + '     css directory');
+log('  ' + color('img', 'blue') + '     images directory');
+log('  ' + color('js', 'blue') + '      javascript directory');
