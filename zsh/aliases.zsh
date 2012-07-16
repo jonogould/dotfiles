832edcaccd42000e08e1d9f8d08f2abc4a5f146f
Wayne Berry <wayneashleyberry@gmail.com>
2012-07-16 13:23:23 +0200

    clean up zshrc

---
+++ zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# ls
+alias ls="ls -FG"
+alias l="ls -lAhG"
+alias ll="ls -lG"
+alias la='ls -AG'
+
+# Git Aliases
+alias br='git checkout -b'
+alias branch='git checkout -b'
+alias ga='git add'
+alias gp='git push'
+alias gs='git status'
+alias gd='git diff'
+alias gdt='git difftool'
+alias gc='git commit -m'
+alias gca='git commit -am'
+
+# Job & Process Management
+alias s='screen'
+alias sr='screen -r'
+
+# Vim inspired key mappings
+alias :e='mvim'
+alias :q='exit'
