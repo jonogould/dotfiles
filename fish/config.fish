e5e3bd9497de5701f54b7f0a56adebb705107cc6
Jono Gould <jono.gould@gmail.com>
2021-05-05 11:46:29 +0200

    feat: adds .tools to path

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,13 @@
 set -gx Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
 set -U EDITOR code
 
+# add paths
+fish_add_path ~/.tools
+
+# gcloud sdk
+if [ -f '/Users/jono/.tools/google-cloud-sdk/path.fish.inc' ]; . '/Users/jono/.tools/google-cloud-sdk/path.fish.inc'; end
+
 # fish configs
 source ~/.dotfiles/fish/alias.fish
 source ~/.dotfiles/fish/functions/take.fish
 source ~/.dotfiles/fish/functions/stake.fish
-
-# gcloud sdk
-if [ -f '/Users/jono/.tools/google-cloud-sdk/path.fish.inc' ]; . '/Users/jono/.tools/google-cloud-sdk/path.fish.inc'; end
